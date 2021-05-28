f60a2d7992fa8584c6d20f46a43c21292de97cdc
huggosantos <huggo.oliveira1@gmail.com>
2021-05-27 22:42:10 -0300

    Comit inicial com configuração do projeto, config JSF,JPA com MySQl e servidor WildFly configurado com o projeto, + criação de Bean para gerenciar o formulario de cadastro de livro

---
+++ src/main/java/br/com/casadocodigo/loja/dao/LivroDao.java
@@ -0,0 +1,17 @@
+package br.com.casadocodigo.loja.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import br.com.casadocodigo.loja.models.Livro;
+
+public class LivroDao {
+	
+	@PersistenceContext
+	private EntityManager manager;
+	
+	public void salvar(Livro livro) {
+		manager.persist(livro);
+	}
+
+}
---
+++ src/main/java/br/com/casadocodigo/loja/models/Livro.java
@@ -0,0 +1,97 @@
+package br.com.casadocodigo.loja.models;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+@Entity
+public class Livro {
+	
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String titulo;
+	@Lob
+	private String descricao;
+	private BigDecimal preco;
+	private Integer numeroPaginas;
+	
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public BigDecimal getPreco() {
+		return preco;
+	}
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+	public Integer getNumeroPaginas() {
+		return numeroPaginas;
+	}
+	public void setNumeroPaginas(Integer numeroPaginas) {
+		this.numeroPaginas = numeroPaginas;
+	}
+	
+	@Override
+	public String toString() {
+		return "Livro [titulo=" + titulo + ", descricao=" + descricao + ", preco=" + preco + ", numeroPaginas="
+				+ numeroPaginas + "]";
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+		result = prime * result + ((numeroPaginas == null) ? 0 : numeroPaginas.hashCode());
+		result = prime * result + ((preco == null) ? 0 : preco.hashCode());
+		result = prime * result + ((titulo == null) ? 0 : titulo.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livro other = (Livro) obj;
+		if (descricao == null) {
+			if (other.descricao != null)
+				return false;
+		} else if (!descricao.equals(other.descricao))
+			return false;
+		if (numeroPaginas == null) {
+			if (other.numeroPaginas != null)
+				return false;
+		} else if (!numeroPaginas.equals(other.numeroPaginas))
+			return false;
+		if (preco == null) {
+			if (other.preco != null)
+				return false;
+		} else if (!preco.equals(other.preco))
+			return false;
+		if (titulo == null) {
+			if (other.titulo != null)
+				return false;
+		} else if (!titulo.equals(other.titulo))
+			return false;
+		return true;
+	}	
+	
+
+}
