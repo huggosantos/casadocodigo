5bc20e236022a00865db6538df4b1f25737138d3
huggosantos <huggo.oliveira1@gmail.com>
2021-06-04 00:44:38 -0300

    Adicionando a implementação da especificação BeanValidation

--- src/main/java/br/com/casadocodigo/loja/models/Livro.java
+++ src/main/java/br/com/casadocodigo/loja/models/Livro.java
@@ -10,19 +10,38 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Length;
+
+
 
 @Entity
 public class Livro {
 	
 	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	
+	@NotBlank 
 	private String titulo;
+	
 	@Lob
+	@Length(min=10)
+	@NotBlank 
 	private String descricao;
+	
+	@DecimalMin("20")
 	private BigDecimal preco;
+	@Min(50)
 	private Integer numeroPaginas;
 	
 	@ManyToMany 
+	@Size(min=1)
+	@NotNull
 	private List<Autor> autores = new ArrayList<>();
 	
 	public String getTitulo() {
