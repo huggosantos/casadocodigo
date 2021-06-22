0796ff645b5e20bb6a94a450b4b18e09f46d0724
huggosantos <huggo.oliveira1@gmail.com>
2021-06-22 01:08:55 -0300

    Ajuste do commit anterior, pegamos a tela de livro-detalhe da casa do código, e criamos o LivroDetalheBean para gerenciar essa tela e criamos um novo metodo no LivroDao para atender essa demanda

--- src/main/java/br/com/casadocodigo/loja/beans/LivroDetalheBean.java
+++ src/main/java/br/com/casadocodigo/loja/beans/LivroDetalheBean.java
@@ -1,5 +1,38 @@
 package br.com.casadocodigo.loja.beans;
 
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+
+import br.com.casadocodigo.loja.dao.LivroDao;
+import br.com.casadocodigo.loja.models.Livro;
+
+@Model
 public class LivroDetalheBean {
 
+	@Inject
+	private LivroDao dao;
+	
+	private Livro livro;
+	
+	private Integer id;
+	
+	public void carregarDetalhe() {
+		this.livro = dao.buscarPorId(id);
+	}
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public void setLivro(Livro livro) {
+		this.livro = livro;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
 }
--- src/main/java/br/com/casadocodigo/loja/dao/LivroDao.java
+++ src/main/java/br/com/casadocodigo/loja/dao/LivroDao.java
@@ -24,18 +24,25 @@ public class LivroDao {
     }
 	
 	public List<Livro> ultimosLancamentos() {
-        String jpql = "select l from Livro l order by l.id desc";
+        String jpql = "select l from Livro l order by l.dataPublicacao desc";
         return manager.createQuery(jpql, Livro.class)
                 .setMaxResults(5)
                 .getResultList();
     }
 
     public List<Livro> demaisLivros() {
-        String jpql = "select l from Livro l order by l.id desc";
+        String jpql = "select l from Livro l order by l.dataPublicacao desc";
         return manager.createQuery(jpql, Livro.class)
                 .setFirstResult(5)
                 .getResultList();
     }
+
+	public Livro buscarPorId(Integer id) {
+		String jpql = "select l from Livro l join fetch l.autores where l.id =:id";
+		return manager.createQuery(jpql, Livro.class)
+				.setParameter("id",id)
+				.getSingleResult();
+	}
 
 
 }
